d3ff77db58b2a967b7b018e1b028faa1b11f945b
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-06-02 14:22:07 -0400

    Add resource and endpoint URLs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Setup resource: https://dev.to/karanpratapsingh/deploy-react-app-to-s3-cloudfront-1cao
+// S3 endpoint URL: http://aws-s3-cloudfront-react.s3-website.us-east-2.amazonaws.com/
+// CloudFront distribution URL: https://ddfpslir92hjk.cloudfront.net/
 function App() {
   return (
     <div className="App">
